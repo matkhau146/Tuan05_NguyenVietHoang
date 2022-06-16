7d996326487701a3222896cf3868310049f54f1e
matkhau146 <69472182+matkhau146@users.noreply.github.com>
2022-06-16 18:50:31 +0700

    18h50

---
+++ Models/Giohang.cs
@@ -0,0 +1,25 @@
+﻿using Tuan05_NguyenVietHoang.Models;
+
+public class Giohang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+
+        [Display(Name = "Tên sách" )]
+        public string tensach { get; set; }
+
+        [Display(Name = "Ảnh bìa" )]
+        public string hinh { get; set; }
+        
+        [Display(Name = "Giá bán" )]
+        public string giaban { get; set; }
+
+        [Display(Name = "Số lượng" )]
+        public string isoluong { get; set; }
+
+        [Display(Name = "Thành tiền" )]
+        public double DThanhtien
+        {
+                    
+        }
+}
